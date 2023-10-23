a40cfaffe76e9ad914a497eee34c0395ec3d1ee2
Polina Gorelik <Polina23707@gmail.com>
2023-10-23 10:23:21 +0300

    second commit for api/Transaction

--- public/js/api/Transaction.js
+++ public/js/api/Transaction.js
@@ -4,12 +4,19 @@
  * Имеет свойство URL со значением '/transaction'
  * */
 class Transaction extends Entity {
-  constructor(url) {
-    super(url);
-    this.url = '/transaction';
-  }
+  
+  static URL = '/transaction';
+
+  // createRequest({
+  //   url: this.URL,
+  //   method: 'DELETE',
+  //   responseType: 'json',
+  //   data,
+  //   callback
+  // })
 }
 
+// ПРОВЕРКА
 // console.log('Transaction start');
 
 // let transaction1 = new Transaction;
@@ -18,3 +25,8 @@ class Transaction extends Entity {
 // transaction1.create({ mail: 'ivan@biz.pro' }, ( err, response ) => console.log(err, response));
 // console.log( transaction1 );
 // console.log('Transaction end');
+
+
+// Transaction.list({ mail: 'ivan@biz.pro' }, ( err, response ) => console.log(err, response));
+// Transaction.create({ mail: 'ivan@biz.pro' }, ( err, response ) => console.log(err, response));
+// Transaction.remove({ mail: 'ivan@biz.pro' }, ( err, response ) => console.log(err, response))
