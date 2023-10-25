321f769ff731dd1a2dedb3d77256c6dd1b22e64a
Polina Gorelik <Polina23707@gmail.com>
2023-10-25 11:46:04 +0300

    new commit for Account.js

--- public/js/api/Account.js
+++ public/js/api/Account.js
@@ -17,24 +17,3 @@ class Account extends Entity {
     });
   }
 }
-
-
-
-// ПРОВЕРКА
-// Account.list({ mail: 'ivan@biz.pro' }, ( err, response ) => console.log(err, response));
-// Account.create({ mail: 'ivan@biz.pro' }, ( err, response ) => console.log(err, response));
-// Account.remove({ mail: 'ivan@biz.pro' }, ( err, response ) => console.log(err, response));
-// // console.log('get');
-// Account.get(2, ( err, response ) => console.log(err, response));
-
-// Entity.get( 21, function ( err, response ) {
-//   console.log(err, response);
-//   // ... получили ответ
-// });
-//  Entity.get( 1, (err, response) => {
-//   console.log( 'Ошибка, если есть', err );
-//   console.log( 'Данные, если нет ошибки', response );
-// });
-// let account1 = new Account;
-// // console.log(account1);
-// account1.get('1', ( err, response ) => console.log(err, response));
