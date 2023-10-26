c7e33127bcc27ae979a50ee9f89c5509ce6d1f2d
Polina Gorelik <Polina23707@gmail.com>
2023-10-26 10:19:48 +0300

    response.success test added

--- public/js/ui/forms/LoginForm.js
+++ public/js/ui/forms/LoginForm.js
@@ -11,8 +11,6 @@ class LoginForm extends AsyncForm {
    * */
   onSubmit(data) {
     User.login(data, (err, response) => {
-      console.log(err);
-      console.log(response);
       if (response.success) {
         App.setState('user-logged');
         App.getModal('login').close();
