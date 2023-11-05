fed32b39299773bf666c282d22c5597cadc0b69b
Polina Gorelik <Polina23707@gmail.com>
2023-11-05 18:11:25 +0300

    reset form in AsyncForm.getData

--- public/js/ui/forms/AsyncForm.js
+++ public/js/ui/forms/AsyncForm.js
@@ -40,31 +40,9 @@ class AsyncForm {
    * }
    * */
   getData() {
-    const form = this.element;
-    const formData = new FormData(form);
-    
-    if (form.getAttribute('id') === 'new-income-form' || form.getAttribute('id') === 'new-expense-form') {
-      let data = Array.from(form.children);
-      let type = data[0];
-      let name = Array.from(data[1].children)[0];
-      let sum = Array.from(data[2].children)[0];
-      let accId = Array.from(data[3].children)[1];
-      let rows = [name, sum, accId];
-      
-      formData['type'] = type.getAttribute('value');
-      rows.forEach((row) => {
-        formData[row.getAttribute('name')] = row.value;
-      })
-      
-
-    } else {
-      Array.from(form.children).forEach((item) => {
-        let name = Array.from(item.children)[0].getAttribute('name');
-        let value = Array.from(item.children)[0].value;
-        formData[name] = value;
-      })
-    }
-      return formData;
+    const formData = new FormData(this.element);
+    this.element.reset();
+    return Object.fromEntries(formData.entries());
   }
 
   onSubmit(options){
